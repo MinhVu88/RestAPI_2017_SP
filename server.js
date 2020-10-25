b5f20e6d66c1d1f90602fa47732bc9c14abebf54
Minh Vu <minhvu72826@gmail.com>
2020-10-25 14:38:11 +0700

    use MongoDB Atlas, instead of Robo 3T

--- server.js
+++ server.js
@@ -4,14 +4,21 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const port = process.env.PORT || 4000;
 const routes = require("./routes/api");
+const mongodb_uri =
+  "mongodb+srv://mainUser:17Maynard04Keenan1964@nodejs.oacgl.mongodb.net/rest_api_tut?retryWrites=true&w=majority";
 
 // connect to mongodb
-mongoose.connect("mongodb://localhost/rest_api_tut_sp", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true
-});
+mongoose
+  .connect(mongodb_uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  })
+  .then(() =>
+    app.listen(port, () => console.log(`server's listening on port ${port}`))
+  )
+  .catch(error => console.log(error));
 mongoose.Promise = global.Promise;
 
 // middlewares
@@ -19,4 +26,7 @@ app.use(cors());
 app.use(express.json({ extended: false }));
 app.use("/api", routes);
 
-app.listen(port, () => console.log(`server's listening on port ${port}`));
+// 404
+app.use((req, res, next) =>
+  res.status(404).json({ msg: "404 Page Not Found" })
+);
