261a90b49651325801f8b9f04c0d1bdf8dc0ccb6
Minh Vu <minhvu72826@gmail.com>
2021-02-08 00:12:51 +0700

    2nd commit

--- client/src/App.js
+++ client/src/App.js
@@ -2,62 +2,61 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [users, setUsers] = useState([]);
-  const [longitude, setLongitude] = useState("");
-  const [latitude, setLatitude] = useState("");
+	const [users, setUsers] = useState([]);
+	const [longitude, setLongitude] = useState("");
+	const [latitude, setLatitude] = useState("");
 
-  const handleSubmit = e => {
-    e.preventDefault();
+	const handleSubmit = e => {
+		e.preventDefault();
 
-    fetch(`/api/users/geo?long=${longitude}&lat=${latitude}`)
-      .then(response => response.json())
-      .then(data => setUsers(data))
-      .catch(error => console.log(error));
+		fetch(`/api/users/geo?long=${longitude}&lat=${latitude}`)
+			.then(response => response.json())
+			.then(data => setUsers(data))
+			.catch(error => console.log(error));
 
-    setLongitude("");
-    setLatitude("");
-  };
+		setLongitude("");
+		setLatitude("");
+	};
 
-  console.log(latitude, longitude, users);
+	console.log(latitude, longitude, users);
 
-  return (
-    <div id="home">
-      <div id="container">
-        <form id="search" onSubmit={handleSubmit}>
-          <label>Enter your Latitude:</label>
-          <input
-            type="text"
-            placeholder="latitude"
-            value={latitude}
-            onChange={e => setLatitude(e.target.value)}
-            required
-          />
-          <label>Enter your Longitude:</label>
-          <input
-            type="text"
-            placeholder="longitude"
-            value={longitude}
-            onChange={e => setLongitude(e.target.value)}
-            required
-          />
-          <input type="submit" value="Find Users" />
-        </form>
-        <ul>
-          {users.length > 0 &&
-            users.map((user, index) => (
-              <li key={index}>
-                <span className={user.available}></span>
-                <span className="name">{user.name}</span>
-                <span className="age">{user.age}</span>
-                <span className="dist">
-                  {Math.floor(user.dist.calculated / 1000)} km
-                </span>
-              </li>
-            ))}
-        </ul>
-      </div>
-    </div>
-  );
+	return (
+		<div id="home">
+			<div id="container">
+				<h1>REST API Tut 2017 - S.P</h1>
+				<form id="search" onSubmit={handleSubmit}>
+					<label>Enter your Latitude:</label>
+					<input
+						type="text"
+						placeholder="latitude"
+						value={latitude}
+						onChange={e => setLatitude(e.target.value)}
+						required
+					/>
+					<label>Enter your Longitude:</label>
+					<input
+						type="text"
+						placeholder="longitude"
+						value={longitude}
+						onChange={e => setLongitude(e.target.value)}
+						required
+					/>
+					<input type="submit" value="Find Users" />
+				</form>
+				<ul>
+					{users.length > 0 &&
+						users.map((user, index) => (
+							<li key={index}>
+								<span className={user.available ? "true" : "false"}></span>
+								<span className="name">{user.name}</span>
+								<span className="age">{user.age} y.o</span>
+								<span className="dist">{Math.floor(user.dist.calculated / 1000)} km</span>
+							</li>
+						))}
+				</ul>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- server.js
+++ server.js
@@ -1,24 +1,23 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
 const port = process.env.PORT || 4000;
 const routes = require("./routes/api");
-const mongodb_uri =
-  "mongodb+srv://mainUser:17Maynard04Keenan1964@nodejs.oacgl.mongodb.net/rest_api_tut?retryWrites=true&w=majority";
+const mongodb_uri = process.env.MONGODB_URI;
 
 // connect to mongodb
 mongoose
-  .connect(mongodb_uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true
-  })
-  .then(() =>
-    app.listen(port, () => console.log(`server's listening on port ${port}`))
-  )
-  .catch(error => console.log(error));
+	.connect(mongodb_uri, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false,
+		useCreateIndex: true
+	})
+	.then(() => app.listen(port, () => console.log(`server's listening on port ${port}`)))
+	.catch(error => console.log(error));
 mongoose.Promise = global.Promise;
 
 // middlewares
@@ -27,6 +26,4 @@ app.use(express.json({ extended: false }));
 app.use("/api", routes);
 
 // 404
-app.use((req, res, next) =>
-  res.status(404).json({ msg: "404 Page Not Found" })
-);
+app.use((req, res, next) => res.status(404).json({ msg: "404 Page Not Found" }));
